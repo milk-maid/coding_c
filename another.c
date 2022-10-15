474cffe7c57183be061c58ade63950a5d157ec84
milk-maid <roqbell@gmail.com>
2022-10-15 12:48:58 +0100

    bugs everywhere

--- another.c
+++ another.c
@@ -1,23 +1,28 @@
 //I'm beginning to love this.
-/*
-  This is just the start
-  This path shall be fruitful 
-  Bello Roqeeb to the word
-*/
+/**
+ * This is just the start
+ * This path shall be fruitful 
+ * Bello Roqeeb to the word
+ */
 #include <stdio.h>
-#include <conio.h>
+//#include <conio.h>
 #include <math.h>
-#define Pi 3.1
-#define MAP Jaguda
-int a = 50;
-void display(); 
-void main()
+#define PI 3.1
+#define MAP "zoo"
+int a = 57;
+int display(); 
+int main()
 {
-   printf("Getting Cool\n");  //all keywords in lowercase 
+   printf("Getting Cool");  //all keywords in lowercase 
    display();
-   getche(); //to hold the screen & get a char
+   //getche(); //to hold the screen & get a char
+   printf("\n%c\n", a);
+   return 0;
 }
-void display()
+int display()
 { 
    printf("\nRoqeeb is gonna rule the world!"); //this lang is case sensitive
-}
+   printf("\n%f", PI);
+   printf("\n%s", MAP);
+   return 0;
+} 
--- trying.c
+++ trying.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
-#include <conio.h>
 
-void main() {
+//#include <conio.h>
+
+int main() {
     char a = '@';
     char b = 98;
     char c = 'z';
     char d = 132;
-    unsigned char e = -143;
+    unsigned char e = 9;
     printf("\n%u", e);
     printf("\n%d", e);
     printf("\n%c", e);
     printf("\n%c", b);
     printf("\n%d", a);
-    printf("\n%c", a);
-    getch();
+    printf("\n%c\n", a);
+    //getch();
+    return 0;
 } 
