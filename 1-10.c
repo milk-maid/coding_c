b0b1e4a92ae61c73ab37483dbf134538c76e9719
milk-maid <roqbell@gmail.com>
2022-10-26 16:31:33 +0000

    playing with loop

---
+++ 1-10.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0(success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 10; i <= 10; i++)
+	{
+		printf("i = %d\n", i);
+		/* printf("JK\n"); */
+	}
+	printf("End Of Program\n");
+}
